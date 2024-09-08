ab84972b8bb4e69a4c8e382226ac96cbf26985d0
yuki-gonzalez <virtual.rolando.yuki@gmail.com>
2024-09-08 06:58:20 -0600

    menu
    menu

--- myscrypt.js
+++ myscrypt.js
@@ -7,14 +7,16 @@ document.querySelector('h2').addEventListener('click', function() {
 });
 const iconContainer = document.querySelector('nav > div');
 const spans = iconContainer.querySelectorAll('span');
-const lista = document.querySelector('ul');
 
 
-console.log(iconContainer);
-spans.forEach(span => {
+const navElement = document.querySelector('nav');
+const spanElements = navElement.querySelectorAll('span');        
+const lista = navElement.querySelector('ul');
+
+spanElements.forEach(span => {
   span.addEventListener('click', () => {
     lista.classList.toggle('open');
-    spans.forEach(span => {
+    spanElements.forEach(span => {
       span.classList.toggle('ocultar');
     });
   });
