7227237fbc919d3ffe1f0f958fe55ec0c9acb9ef
yuki-gonzalez <virtual.rolando.yuki@gmail.com>
2024-09-11 03:42:23 -0600

    ultima version
    ultima version

--- myscrypt.js
+++ myscrypt.js
@@ -1,6 +1,10 @@
 const navElement = document.querySelector('nav');
 const spanElements = navElement.querySelectorAll('span');        
 const lista = navElement.querySelector('ul');
+const liElements = navElement.querySelectorAll('li');
+
+const certificadosFlex = document.querySelector('.certificados-flex');
+const mimodalCards = certificadosFlex.querySelectorAll('.mimodal-card');
 
 spanElements.forEach(span => {
   span.addEventListener('click', () => {
@@ -8,6 +12,32 @@ spanElements.forEach(span => {
     spanElements.forEach(span => {
       span.classList.toggle('ocultar');
     });
+  });
+});
+
+mimodalCards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('zoom-card');
+  });
+});
+
+liElements.forEach((li, index) => {
+  li.addEventListener('click', () => {
+	lista.classList.toggle('open');
+	spanElements.forEach(span => {
+      span.classList.toggle('ocultar');
+    });
+	if ((index + 1) === 1) {
+      document.getElementById('id_contactos').scrollIntoView({ behavior: 'smooth' });
+    } else if ((index + 1) === 2) {
+      document.getElementById('id_servicios').scrollIntoView({ behavior: 'smooth' });
+    } else if ((index + 1) === 3) {
+      document.getElementById('id_skill').scrollIntoView({ behavior: 'smooth' });
+    } else if ((index + 1) === 4) {
+      document.getElementById('id_experiencia').scrollIntoView({ behavior: 'smooth' });
+    } else if ((index + 1) === 5) {
+      document.getElementById('id_certificados').scrollIntoView({ behavior: 'smooth' });
+    }
   });
 });
 
